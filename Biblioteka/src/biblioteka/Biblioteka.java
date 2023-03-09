efcf2ddf499dbb357d3623a09d9981649b16a4ce
BorisZivanovic <borisziv00@gmail.com>
2023-03-09 09:23:41 +0100

    Dodata logicka kontrola u klasu Biblioteka.

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -1,33 +1,50 @@
 package biblioteka;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import biblioteka.interfejs.BibliotekaInterfejs;
 
 public class Biblioteka implements BibliotekaInterfejs {
 
+	private List<Knjiga> knjige = new ArrayList<Knjiga>();
+	
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
-		// TODO Auto-generated method stub
-
+		
+		if(knjiga == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+		
+		if(knjige.contains(knjiga))
+			throw new IllegalArgumentException("Knjiga je duplikat");
+		
+		knjige.add(knjiga);
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
-		// TODO Auto-generated method stub
-
+		knjige.remove(knjiga);
 	}
 
 	@Override
 	public List<Knjiga> vratiSveKnjige() {
-		// TODO Auto-generated method stub
-		return null;
+				return knjige;
 	}
 
 	@Override
 	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if(autor == null && isbn <= 0 && naslov == null && izdavac == null)
+			throw new IllegalArgumentException("Morate uneti bar jedan kriterijum");
+		
+		List<Knjiga> rezultati = new ArrayList<Knjiga>();
+		
+		if(naslov != null)
+		for (Knjiga knjiga : knjige) {
+			if(knjiga.getNaslov().toLowerCase().contains(naslov.toLowerCase().trim()))
+				rezultati.add(knjiga);
+		}
+		return rezultati;
 	}
 
 }
